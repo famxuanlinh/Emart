d2b6b7288be46118b3a497f09f4aef35a2233a75
famxuanlinh <famxuanlinh@gmail.com>
2022-04-04 13:31:34 +0700

    product

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import Products from './Products';
 
 const Home = () => {
     return (
         <div className='hero'>
-            <div class="card bg-dark text-white">
-                <img src="/assets/bg.jpg" class="card-img" alt="..."/>
-                    <div class="card-img-overlay">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text">Last updated 3 mins ago</p>
+            <div className="card bg-dark text-white border-0">
+                <img src="/assets/bg.jpg" className="card-img" alt="Background" height="550px"/>
+                    <div className="card-img-overlay d-flex flex-column justify-content-center">
+                        <div className="container">
+                        <h5 className="card-title display-3 mb-0">NEW SESION CANIVALS</h5>
+                        <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+                        </div>
                     </div>
             </div>
+            <Products />
         </div>
     );
 }
